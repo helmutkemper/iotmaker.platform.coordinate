af18862e53086d684e23fc5fd76d55bc3e96295e
Helmut Kemper <helmut.kemper@gmail.com>
2021-06-20 14:24:45 -0300

    refactoring

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.coordinate
+
+go 1.16
+
+require github.com/helmutkemper/iotmaker.santa_isabel_theater.commonTypes v0.0.0-20210620171610-854bfc3dc13d
--- typeDensityInterface.go
+++ typeDensityInterface.go
@@ -1,14 +1,18 @@
 package iotmaker_platform_coordinate
 
+import (
+	commonTypes "github.com/helmutkemper/iotmaker.santa_isabel_theater.commonTypes"
+)
+
 type IDensity interface {
-	Set(value float64)
-	SetInt(value int)
-	Add(value int)
-	Sub(value int)
-	SetDensityFactor(value interface{})
-	Int() int
-	Float64() float64
-	Float32() float32
-	Float() float64
+	Set(value commonTypes.Number)
+	SetInt(value commonTypes.Number)
+	Add(value commonTypes.Number)
+	Sub(value commonTypes.Number)
+	SetDensityFactor(value commonTypes.Number)
+	Int() commonTypes.Number
+	Float64() commonTypes.Number
+	Float32() commonTypes.Number
+	Float() commonTypes.Number
 	String() string
 }
